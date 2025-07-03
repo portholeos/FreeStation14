46761ab9b58b143827bb854257af1dd361c04891
portfiend <109661617+portfiend@users.noreply.github.com>
2025-07-03 01:34:54 -0400

    fix: whacktarget serialization

--- Content.Shared/_FREE/Arcade/Games/WhackGame/SharedWhackGameArcade.Classes.cs
+++ Content.Shared/_FREE/Arcade/Games/WhackGame/SharedWhackGameArcade.Classes.cs
@@ -3,26 +3,26 @@ using Robust.Shared.Utility;
 
 namespace Content.Shared.FREE.Arcade.Games.WhackGame;
 
-[Serializable, NetSerializable]
-public record WhackTarget
+[DataDefinition, Serializable, NetSerializable]
+public partial record WhackTarget
 {
     /// <summary>
     ///     The sprite this target uses.
     /// </summary>
-    [DataField]
-    public required SpriteSpecifier Sprite;
+    [DataField(required: true)]
+    public SpriteSpecifier Sprite;
 
     /// <summary>
     ///     How many points this target gives (or takes away) when you hit it.
     /// </summary>
-    [DataField]
-    public required int Score;
+    [DataField(required: true)]
+    public int Score;
 
     /// <summary>
     ///     The sprite this target uses when it is hit.
     /// </summary>
     [DataField]
-    public SpriteSpecifier? HitSprite;
+    public SpriteSpecifier? HitSprite = null;
 
     /// <summary>
     ///     Whether this target is considered a "friend" (i.e. you should not hit it.)
